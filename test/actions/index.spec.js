1ffc2f82c5e4aab1635efdaedfac4e7ebe974411
Harri Lehtola <peruukki@hotmail.com>
2016-08-28 23:33:45 +0200

    Add tests for async Redux Thunk actions

--- test/actions/index.spec.js
+++ test/actions/index.spec.js
@@ -1,10 +1,28 @@
 import expect from 'expect';
+import nock from 'nock';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import * as actions from '../../src/actions';
+
+const mockStore = configureStore([thunk]);
 
 describe('actions', () => {
 
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
   describe('index', () => {
     const username = 'user';
+    const repositories = [
+      { name: 'repo1', description: 'desc1', html_url: 'url1', extra_field: 'extra1' },
+      { name: 'repo2', description: 'desc2', html_url: 'url2', extra_field: 'extra2' }
+    ];
+    const normalizedRepositories = [
+      { name: 'repo1', description: 'desc1', link: 'url1' },
+      { name: 'repo2', description: 'desc2', link: 'url2' }
+    ];
 
     it('fetchRepositoriesRequest should create the FETCH_REPOSITORIES_REQUEST action', () => {
       expect(actions.fetchRepositoriesRequest(username)).toEqual({
@@ -14,18 +32,10 @@ describe('actions', () => {
     });
 
     it('fetchRepositoriesSuccess should create the FETCH_REPOSITORIES_SUCCESS action with normalized repositories', () => {
-      const repositories = [
-        { name: 'repo1', description: 'desc1', html_url: 'url1', extra_field: 'extra1' },
-        { name: 'repo2', description: 'desc2', html_url: 'url2', extra_field: 'extra2' }
-      ];
-
       expect(actions.fetchRepositoriesSuccess(username, repositories)).toEqual({
         type: 'FETCH_REPOSITORIES_SUCCESS',
         username,
-        repositories: [
-          { name: 'repo1', description: 'desc1', link: 'url1' },
-          { name: 'repo2', description: 'desc2', link: 'url2' }
-        ]
+        repositories: normalizedRepositories
       });
     });
 
@@ -48,6 +58,45 @@ describe('actions', () => {
         error
       });
     });
-  });
 
+    it('fetchRepositories should make a GitHub API request and dispatch request and success response actions', () => {
+      const apiStatus = 200;
+      const apiBody = repositories;
+      const expectedActions = [
+        { type: 'FETCH_REPOSITORIES_REQUEST', username },
+        { type: 'FETCH_REPOSITORIES_SUCCESS', username, repositories: normalizedRepositories }
+      ];
+
+      return assertAsyncFetchActions(apiStatus, apiBody, expectedActions);
+    });
+
+    it('fetchRepositories should make a GitHub API request and dispatch request and failure response actions', () => {
+      const apiStatus = 403;
+      const apiBody = {
+        message: 'API rate limit exceeded for xxx.xxx.xxx.xxx. (But here\'s the good news: ' +
+                 'Authenticated requests get a higher rate limit. Check out the documentation for more details.)',
+        documentation_url: 'https://developer.github.com/v3/#rate-limiting'
+      };
+      const expectedActions = [
+        { type: 'FETCH_REPOSITORIES_REQUEST', username },
+        { type: 'FETCH_REPOSITORIES_FAILURE', username, error: 'Error: Forbidden' }
+      ];
+
+      return assertAsyncFetchActions(apiStatus, apiBody, expectedActions);
+    });
+
+    const assertAsyncFetchActions = (apiStatus, apiBody, expectedActions) => {
+      // Mock GitHub API response
+      nock('https://api.github.com')
+        .get(`/users/${username}/repos`)
+        .reply(apiStatus, apiBody);
+
+      const store = mockStore();
+
+      return store.dispatch(actions.fetchRepositories(username))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+        });
+    };
+  });
 });
